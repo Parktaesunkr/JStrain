74c6f0d28d77c270204ec9634ad600a4d55f0075
Parktaesunkr <spcsm5452@naver.com>
2023-06-01 17:33:25 +0900

    js 기초연습  2일차

---
+++ js02/js02_08.js
@@ -0,0 +1,4 @@
+console.log("I'm in Jeju");
+console.log('Sewha ocean is wonderful');
+console.log('Have you ever Makgeolli?');
+console.log("This is the first line\nAnd this is the second");
---
+++ js02/js02_09.js
@@ -0,0 +1,3 @@
+
+console.log(7>3);
+console.log(7<3);
