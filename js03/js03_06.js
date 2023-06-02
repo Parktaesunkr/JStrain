af186886108f9f8419ee8747f1ad39f09c241fee
Parktaesunkr <spcsm5452@naver.com>
2023-06-02 18:07:30 +0900

    js 기초연습 3일차

---
+++ js03/js03_06.js
@@ -0,0 +1,15 @@
+console.log(true&&true);
+console.log(true&&false);
+console.log('문장'=='문장'&&5 == 5);
+console.log(5==5&&'다른 문장1'=='다른 문장2');
+
+console.log(true||false);
+console.log(false||false);
+console.log('문장'=='문장'||5==10);
+
+console.log(!true);
+console.log(!false);
+console.log(!5);
+console.log(!'문장');
+console.log(!!5);
+console.log(!!'문장');
