c7124c6abfb8acf954a17863ba91f670b3da6db5
Parktaesunkr <spcsm5452@naver.com>
2023-06-05 18:02:32 +0900

    js 기초 연습 4일차

---
+++ js04/js04_08.js
@@ -0,0 +1,9 @@
+let greeting_expression = function(name){
+    console.log('Hi, '+name);
+}
+function greeting_declaration(name){
+    console.log('Hi, '+name);
+}
+
+greeting_expression('Chloe');
+greeting_declaration('Chloe');
